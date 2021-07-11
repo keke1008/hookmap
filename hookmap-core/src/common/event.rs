0fcdf4ccfc783bd1fc66e240d6d3aa0933b72ed9
keke1008 <keke2222337@gmail.com>
2021-07-11 22:46:29 +0900

    added document for event.rs

--- hookmap-core/src/common/event.rs
+++ hookmap-core/src/common/event.rs
@@ -1,8 +1,12 @@
 use std::sync::mpsc::Sender;
 
+/// Indicates whether to pass the generated event to the next program or not .
 #[derive(Debug, Clone, Copy)]
 pub enum EventBlock {
+    /// Do not pass the generated event to the next program.
     Block,
+
+    /// Pass the generated event to the next program.
     Unblock,
 }
 
@@ -31,14 +35,23 @@ impl Drop for BlockInputTx {
     }
 }
 
+/// Information about the generated event.
+/// When dropped, this will send whether to pass the event to the next program or not
+/// to the thread where the hook is installed.
 #[derive(Debug)]
 pub struct EventDetail<T, A> {
+    /// Target of the generated event.
     pub target: T,
+
+    /// Action of the generated event.
     pub action: A,
+
+    /// When dropped, this will send an `EventBlock` to the thread where the hook is installed.
     block_input_tx: BlockInputTx,
 }
 
 impl<T, A> EventDetail<T, A> {
+    /// Creates a new `Event<T, A>`.
     pub fn new(target: T, action: A, block_input_tx: Sender<EventBlock>) -> Self {
         Self {
             target,
@@ -47,6 +60,25 @@ impl<T, A> EventDetail<T, A> {
         }
     }
 
+    /// Returns a mutable reference to the value that will be sent to the thread where the hook is
+    /// installed.
+    ///
+    /// If you want to block the event, assign `EventBlock::Block`,
+    /// otherwise, assign `EventBlock::Unblock`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use hookmap::common::EventBlock;
+    ///
+    /// fn block_event(event: &mut Event) {
+    ///     event.block_input_mut() = EventBlock::Block;
+    /// }
+    ///
+    /// fn unblock_event(event: &mut Event) {
+    ///     event.block_input_mnt() = EventBlock::Unblock;
+    /// }
+    /// ```
     pub fn block_input_mut(&mut self) -> &mut EventBlock {
         self.block_input_tx.block_input_mut()
     }
