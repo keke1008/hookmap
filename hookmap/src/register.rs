a76f3cc8a17037f518cb8b240e78ccb4044441eb
keke1008 <keke2222337@gmail.com>
2021-07-24 16:30:43 +0900

    add docs for register.rs

--- hookmap/src/register.rs
+++ hookmap/src/register.rs
@@ -7,6 +7,7 @@ use derive_new::new;
 use hookmap_core::{Key, MouseInput};
 use std::sync::{Arc, Mutex};
 
+/// A struct to register keyboard handlers.
 #[derive(new, Debug)]
 pub struct KeyboardRegister {
     key: Key,
@@ -15,6 +16,16 @@ pub struct KeyboardRegister {
 }
 
 impl KeyboardRegister {
+    /// Registers a handler called when the specified key is pressed.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use hookmap::{Hook, Key};
+    /// let hook = Hook::new();
+    /// hook.bind_key(Key::A).on_press(|_| println!("The A key is pressed"));
+    /// ```
+    ///
     pub fn on_press<F>(self, callback: F)
     where
         F: FnMut(EventInfo<()>) + Send + 'static,
@@ -28,6 +39,25 @@ impl KeyboardRegister {
             .push(callback, self.modifier);
     }
 
+    /// Registers a handler called when the specified key is pressed or released.
+    ///
+    /// # Arguments
+    ///
+    /// * `callback` - A function that takes `EventInfo` containing whether the specified key
+    /// is pressed or released as an argument.
+    ///
+    /// # Example
+    /// ```
+    /// use hookmap::{Button, Hook, Key};
+    /// let hook = Hook::new();
+    /// hook.bind_key(Key::A).on_press_or_release(|event| {
+    ///     match event.info {
+    ///         Button::Press => println!("The A key is pressed"),
+    ///         Button::Release => println!("The A key is released"),
+    ///     };
+    /// });
+    /// ```
+    ///
     pub fn on_press_or_release<F>(self, callback: F)
     where
         F: FnMut(EventInfo<Button>) + Send + 'static,
@@ -41,6 +71,16 @@ impl KeyboardRegister {
             .push(callback, self.modifier);
     }
 
+    /// Registers a handler called when the specified key is released.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use hookmap::{Hook, Key};
+    /// let hook = Hook::new();
+    /// hook.bind_key(Key::A).on_release(|_| println!("The A key is released"));
+    /// ```
+    ///
     pub fn on_release<F>(self, callback: F)
     where
         F: FnMut(EventInfo<()>) + Send + 'static,
@@ -63,6 +103,7 @@ fn is_button(mouse: MouseInput) -> bool {
         || mouse == MouseInput::SideButton2
 }
 
+/// A struct to register mouse handlers.
 #[derive(new, Debug)]
 pub struct MouseRegister {
     mouse: MouseInput,
@@ -71,6 +112,20 @@ pub struct MouseRegister {
 }
 
 impl MouseRegister {
+    /// Registers a handler called when the mouse cursor is moved.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `MouseInput::Move` is not specified.
+    ///
+    /// # Example
+    /// ```
+    /// use hookmap::{Hook, MouseInput};
+    /// let hook = Hook::new();
+    /// hook.bind_mouse(MouseInput::Move).on_move(|event| {
+    ///     println!("Current mouse cursor position(x, y): ({}, {})", event.info.0, event.info.1);
+    /// });
+    /// ```
     pub fn on_move<F>(self, callback: F)
     where
         F: FnMut(EventInfo<(i32, i32)>) + Send + 'static,
@@ -84,6 +139,21 @@ impl MouseRegister {
             .push(callback, self.modifier);
     }
 
+    /// Registers a handler called when the specified mouse button is pressed.
+    ///
+    /// # Panics
+    ///
+    /// Panics if no mouse button is specified.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use hookmap::{Hook, MouseInput};
+    /// let hook = Hook::new();
+    /// hook.bind_mouse(MouseInput::LButton)
+    ///     .on_press(|_| println!("The left mouse button is pressed"));
+    /// ```
+    ///
     pub fn on_press<F>(self, callback: F)
     where
         F: FnMut(EventInfo<()>) + Send + 'static,
@@ -98,6 +168,29 @@ impl MouseRegister {
             .push(callback, self.modifier);
     }
 
+    /// Registers a handler called when the specified mouse button is pressed or released.
+    ///
+    /// # Arguments
+    ///
+    /// * `callback` - A function that takes `EventInfo` containing whether the specified mouse
+    /// button is pressed or released as an argument.
+    ///
+    /// # Panics
+    ///
+    /// Panics if no mouse button is specified.
+    ///
+    /// # Example
+    /// ```
+    /// use hookmap::{Button, Hook, MouseInput};
+    /// let hook = Hook::new();
+    /// hook.bind_mouse(MouseInput::LButton).on_press_or_release(|event| {
+    ///     match event.info {
+    ///         Button::Press => println!("The left mouse button is pressed"),
+    ///         Button::Release => println!("The left mouse button is released"),
+    ///     };
+    /// });
+    /// ```
+    ///
     pub fn on_press_or_release<F>(self, callback: F)
     where
         F: FnMut(EventInfo<Button>) + Send + 'static,
@@ -112,6 +205,20 @@ impl MouseRegister {
             .push(callback, self.modifier);
     }
 
+    /// Registers a handler called when the specified mouse button is released.
+    ///
+    /// # Panics
+    ///
+    /// Panics if no mouse button is specified.
+    ///
+    /// # Example
+    /// ```
+    /// use hookmap::{Hook, MouseInput};
+    /// let hook = Hook::new();
+    /// hook.bind_mouse(MouseInput::LButton)
+    ///     .on_release(|_| println!("The left mouse button is released"));
+    /// ```
+    ///
     pub fn on_release<F>(self, callback: F)
     where
         F: FnMut(EventInfo<()>) + Send + 'static,
@@ -126,6 +233,26 @@ impl MouseRegister {
             .push(callback, self.modifier);
     }
 
+    /// Registers a handler called when the mouse wheel is rotated.
+    ///
+    /// # Arguments
+    ///
+    /// * `callback` - A function that takes `EventInfo` containing a rotation speed of the mouse
+    /// wheel as an argument.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `MouseInput::Wheel` is not specified.
+    ///
+    /// # Example
+    /// ```
+    /// use hookmap::{Hook, MouseInput};
+    /// let hook = Hook::new();
+    /// hook.bind_mouse(MouseInput::Wheel).on_rotate(|event| {
+    ///     println!("Mouse wheel rotation speed: {}", event.info);
+    /// });
+    /// ```
+    ///
     pub fn on_rotate<F>(self, callback: F)
     where
         F: FnMut(EventInfo<i32>) + Send + 'static,
