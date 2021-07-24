1652d8fc3efa37c5be40e705adb85161703397ae
keke1008 <keke2222337@gmail.com>
2021-07-24 16:49:44 +0900

    add visibility

--- hookmap/src/lib.rs
+++ hookmap/src/lib.rs
@@ -1,9 +1,9 @@
 pub mod event;
-pub mod handler;
+mod handler;
 pub mod hook;
-pub mod modifier;
+mod modifier;
 pub mod register;
 
 pub use event::{Button, EventInfo};
 pub use hook::Hook;
-pub use hookmap_core::{EmulateKeyboardInput, EmulateMouseInput, Key, MouseInput as Mouse};
+pub use hookmap_core::{EmulateKeyboardInput, EmulateMouseInput, Key, MouseInput};
