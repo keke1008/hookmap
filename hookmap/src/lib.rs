3eb391cf05295e7f5a95ad77b4c775519ddbd41a
keke1008 <keke2222337@gmail.com>
2021-08-01 02:06:30 +0900

    Add docs

--- hookmap/src/lib.rs
+++ hookmap/src/lib.rs
@@ -1,6 +1,30 @@
 //! # Hookmap
 //!
 //! Register hotkeys and emulate input.
+//!
+//! ## Handler order.
+//!
+//! Handlers are called in the order in which they are registered.
+//!
+//! ## EventBlock
+//!
+//! [`EventBlock`] can be used in the following cases,
+//! and the higier it is, the higher the priority.
+//!
+//! * The argument of [`modifier_key`] or [`modifier_mouse_button`]
+//! * The closue argument passed to [`ButtonRegister::on_*`]
+//!     ( call [`block_event`], or [`unblock_event`] to change [`EventBlock`])
+//!
+//! [`modifier_key`]: crate::SelectHandleTarget::modifier_key
+//! [`modifier_mouse_button`]: crate::SelectHandleTarget::modifier_mouse_button
+//! [`ButtonRegister::on_*`]: crate::interface::ButtonRegister
+//! [`block_event`]: crate::EventInfo::block_event
+//! [`unblock_event`]: crate::EventInfo::unblock_event
+//!
+//! ## Feature flags
+//!
+//! * `block-input-event`: Set the default `EventBlock` value to `EventBlock::Block`.
+//!
 
 pub mod event;
 pub mod interface;
