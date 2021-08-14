7b2fbb03b0acdd7bc1b2c7617c716f2e1412efb9
keke1008 <keke2222337@gmail.com>
2021-08-15 01:39:16 +0900

    Hide Variants of Cond

--- hookmap/src/cond.rs
+++ hookmap/src/cond.rs
@@ -3,43 +3,46 @@ use hookmap_core::ButtonState;
 use std::{fmt::Debug, sync::Arc};
 
 #[derive(Clone)]
-pub enum Cond {
+enum _Cond {
     Pressed(ButtonWithState),
     Released(ButtonWithState),
     Callback(Arc<dyn Fn() -> bool + Send + Sync>),
 }
 
+#[derive(Clone)]
+pub struct Cond(_Cond);
+
 impl Cond {
     pub(crate) fn is_satisfied(&self) -> bool {
-        match self {
-            Cond::Pressed(button) => button.is_pressed(),
-            Cond::Released(button) => !button.is_pressed(),
-            Cond::Callback(callback) => callback(),
+        match &self.0 {
+            _Cond::Pressed(button) => button.is_pressed(),
+            _Cond::Released(button) => !button.is_pressed(),
+            _Cond::Callback(callback) => callback(),
         }
     }
 
     pub fn pressed(button: impl DownCastableButtonState) -> Self {
         let button = Box::new(button).into_button_with_state();
-        Self::Pressed(button)
+        Self(_Cond::Pressed(button))
     }
 
     pub fn released(button: impl DownCastableButtonState) -> Self {
         let button = Box::new(button).into_button_with_state();
-        Self::Released(button)
+        Self(_Cond::Released(button))
     }
 
-    pub fn cond<F: 'static + Fn() -> bool + Send + Sync>(callback: F) -> Self {
-        Self::Callback(Arc::new(callback))
+    pub fn callback<F: 'static + Fn() -> bool + Send + Sync>(callback: F) -> Self {
+        Self(_Cond::Callback(Arc::new(callback)))
     }
 }
 
 impl Debug for Cond {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_fmt(format_args!("{}::", std::any::type_name::<Self>()))?;
-        match self {
-            Cond::Pressed(button) => f.write_fmt(format_args!("Press({:?})", button)),
-            Cond::Released(button) => f.write_fmt(format_args!("Release({:?})", button)),
-            Cond::Callback(_) => f.write_str("callback"),
+        match &self.0 {
+            _Cond::Pressed(button) => f.write_fmt(format_args!("Press({:?})", button)),
+            _Cond::Released(button) => f.write_fmt(format_args!("Release({:?})", button)),
+            _Cond::Callback(_) => f.write_str("callback"),
         }
     }
 }
