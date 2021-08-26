8cb76efa21b457d5acd9fcad3b8c72b8095e177e
keke1008 <keke2222337@gmail.com>
2021-08-26 12:23:43 +0900

    Update `button_name`

--- hookmap/src/macros.rs
+++ hookmap/src/macros.rs
@@ -1,26 +1,36 @@
 /// Expands button names.
 ///
+/// If the argument is enclosed in square brackets, it will be expanded without any action.
+///
 /// # Example
 /// ```
 /// use hookmap::*;
 /// assert_eq!(Button::Key0, button_name!(0));
 /// assert_eq!(Button::A, button_name!(A));
+///
+/// let button_a = Button::A;
+/// assert_eq!(Button::A, button_name!([button_a]));
 /// ```
 ///
 #[rustfmt::skip]
 #[macro_export]
 macro_rules! button_name {
-    ($button:ident) => (Button::$button);
-    (0)             => (Button::Key0);
-    (1)             => (Button::Key1);
-    (2)             => (Button::Key2);
-    (3)             => (Button::Key3);
-    (4)             => (Button::Key4);
-    (5)             => (Button::Key5);
-    (6)             => (Button::Key6);
-    (7)             => (Button::Key7);
-    (8)             => (Button::Key8);
-    (9)             => (Button::Key9);
+    ([$button:expr]) => ($button);
+    ($button:ident)  => ($crate::Button::$button);
+    (0)              => ($crate::Button::Key0);
+    (1)              => ($crate::Button::Key1);
+    (2)              => ($crate::Button::Key2);
+    (3)              => ($crate::Button::Key3);
+    (4)              => ($crate::Button::Key4);
+    (5)              => ($crate::Button::Key5);
+    (6)              => ($crate::Button::Key6);
+    (7)              => ($crate::Button::Key7);
+    (8)              => ($crate::Button::Key8);
+    (9)              => ($crate::Button::Key9);
+    (;)              => ($crate::Button::SemiColon);
+    (-)              => ($crate::Button::Minus);
+    (/)              => ($crate::Button::Slash);
+
 }
 
 /// Presses and releases the keys in sequence.
