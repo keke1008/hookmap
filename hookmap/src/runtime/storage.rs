b59739705974355144a42f810caae114f89b6bb3
keke1008 <keke2222337@gmail.com>
2021-09-28 14:09:25 +0900

    Split the struct Hook into OnPressHook and OnReleaseHook

--- hookmap/src/runtime/storage.rs
+++ hookmap/src/runtime/storage.rs
@@ -5,44 +5,58 @@ use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 
 #[derive(Debug)]
-pub(super) enum HookKind {
-    OnModifierPressed {
+pub(super) struct OnPressHook {
+    pub(super) action: Action<ButtonEvent>,
+    pub(super) modifier_keys: Arc<ModifierKeys>,
+    pub(super) activated: Arc<AtomicBool>,
+    pub(super) event_block: EventBlock,
+}
+
+impl OnPressHook {
+    pub(super) fn new(
+        action: impl Into<Action<ButtonEvent>>,
         modifier_keys: Arc<ModifierKeys>,
         activated: Arc<AtomicBool>,
-    },
+        event_block: impl Into<EventBlock>,
+    ) -> Self {
+        Self {
+            action: action.into(),
+            modifier_keys,
+            activated,
+            event_block: event_block.into(),
+        }
+    }
 
-    OnModifierReleased {
-        activated: Arc<AtomicBool>,
-    },
-
-    Solo,
+    pub(super) fn satisfies_condition(&self) -> bool {
+        self.modifier_keys.satisfies_condition() && {
+            self.activated.store(true, Ordering::SeqCst);
+            true
+        }
+    }
 }
 
 #[derive(Debug)]
-pub(crate) struct Hook {
+pub(super) struct OnReleaseHook {
     pub(super) action: Action<ButtonEvent>,
+    pub(super) activated: Arc<AtomicBool>,
     pub(super) event_block: EventBlock,
-    pub(super) kind: HookKind,
 }
 
-impl Hook {
-    pub(super) fn is_satisfied(&self) -> bool {
-        match &self.kind {
-            HookKind::Solo => true,
-            HookKind::OnModifierReleased { activated, .. } => {
-                activated.swap(false, Ordering::SeqCst)
-            }
-            HookKind::OnModifierPressed {
-                modifier_keys: modifier,
-                activated,
-                ..
-            } => {
-                modifier.satisfies_condition() && {
-                    activated.store(true, Ordering::SeqCst);
-                    true
-                }
-            }
+impl OnReleaseHook {
+    pub(super) fn new(
+        action: impl Into<Action<ButtonEvent>>,
+        activated: Arc<AtomicBool>,
+        event_block: impl Into<EventBlock>,
+    ) -> Self {
+        Self {
+            action: action.into(),
+            activated,
+            event_block: event_block.into(),
         }
+    }
+
+    pub(super) fn satisfies_condition(&self) -> bool {
+        self.activated.swap(false, Ordering::SeqCst)
     }
 }
 
@@ -53,15 +67,15 @@ pub struct MouseHook<E> {
     pub(super) event_block: EventBlock,
 }
 
-pub(super) type ButtonStorage = HashMap<Button, Vec<Arc<Hook>>>;
+pub(super) type ButtonStorage<T> = HashMap<Button, Vec<Arc<T>>>;
 pub(super) type MouseStorage<E> = Vec<Arc<MouseEventHandler<E>>>;
 pub(super) type MouseCursorStorage = MouseStorage<MouseCursorEvent>;
 pub(super) type MouseWheelStorage = MouseStorage<MouseWheelEvent>;
 
 #[derive(Default, Debug)]
 pub(super) struct Storage {
-    pub(super) on_press: ButtonStorage,
-    pub(super) on_release: ButtonStorage,
+    pub(super) on_press: ButtonStorage<OnPressHook>,
+    pub(super) on_release: ButtonStorage<OnReleaseHook>,
     pub(super) mouse_cursor: MouseCursorStorage,
     pub(super) mouse_wheel: MouseWheelStorage,
 }
