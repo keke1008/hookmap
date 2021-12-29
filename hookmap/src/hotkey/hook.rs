7b562f45d6bacfe3276e9b2a8fec5a61341fe43e
keke1008 <keke2222337@gmail.com>
2021-12-29 20:03:32 +0900

    Add method for ModifierKeys

--- hookmap/src/hotkey/hook.rs
+++ hookmap/src/hotkey/hook.rs
@@ -14,6 +14,28 @@ pub(super) struct ModifierKeys {
 }
 
 impl ModifierKeys {
+    pub(super) fn new(pressed: Vec<ButtonSet>, released: Vec<ButtonSet>) -> Self {
+        Self { pressed, released }
+    }
+
+    pub(super) fn add_pressed(&self, button: ButtonSet) -> Self {
+        let mut pressed = self.pressed.clone();
+        pressed.push(button);
+        Self {
+            pressed,
+            released: self.released.clone(),
+        }
+    }
+
+    pub(super) fn add_released(&self, button: ButtonSet) -> Self {
+        let mut released = self.released.clone();
+        released.push(button);
+        Self {
+            pressed: self.pressed.clone(),
+            released,
+        }
+    }
+
     fn meets_conditions(&self) -> bool {
         self.pressed.iter().all(|button| button.is_pressed())
             && self.released.iter().all(|button| button.is_released())
