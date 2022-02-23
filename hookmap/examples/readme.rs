70cd5c14fb03a5ae8b70df3833dca997bab6b05d
keke1008 <keke2222337@gmail.com>
2022-02-23 14:22:08 +0900

    Change readme to use method chain

--- hookmap/examples/readme.rs
+++ hookmap/examples/readme.rs
@@ -4,19 +4,20 @@ fn main() {
     let hotkey = Hotkey::new();
 
     // Remap H,J,K,L keys as in vim.
-    hotkey.remap(Button::H, Button::LeftArrow);
-    hotkey.remap(Button::J, Button::DownArrow);
-    hotkey.remap(Button::K, Button::UpArrow);
-    hotkey.remap(Button::L, Button::RightArrow);
+    hotkey
+        .remap(Button::H, Button::LeftArrow)
+        .remap(Button::J, Button::DownArrow)
+        .remap(Button::K, Button::UpArrow)
+        .remap(Button::L, Button::RightArrow);
 
     // You can define hotkeys that work only when specific keys are pressed or released.
     let modified = hotkey.add_modifiers(buttons!(LCtrl, !RShift));
 
-    modified.on_press(Button::Space, |_| seq!(with(LCtrl), A).send());
-
-    modified.on_release(buttons!(A, B), |event: ButtonEvent| {
-        seq!(with(LShift), [event.target]).send()
-    });
+    modified
+        .on_press(Button::Space, |_| seq!(with(LCtrl), A).send())
+        .on_release(buttons!(A, B), |event: ButtonEvent| {
+            seq!(with(LShift), [event.target]).send()
+        });
 
     hotkey.install();
 }
